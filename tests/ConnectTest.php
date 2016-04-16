2be7d09394a6ead69568eaa23165d89867e13e58
Eric Lamb <eric@mithra62.com>
2016-04-15 19:13:17 -0700

    Adds test

---
+++ tests/ConnectTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Jaeger
+ *
+ * @copyright	Copyright (c) 2015-2016, mithra62
+ * @link		http://jaeger-app.com
+ * @version		1.0
+ * @filesource 	./tests/ConnectTest.php
+ */
+namespace JaegerApp\tests;
+
+use JaegerApp\Validate;
+use JaegerApp\Validate\Rules\Dropbox\Connect;
+
+/**
+ * mithra62 - Valiate object Unit Tests
+ *
+ * Contains all the unit tests for the \mithra62\Valiate object
+ *
+ * @package mithra62\Tests
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class ConnectTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Tests the name of the rule
+     */
+    public function testName()
+    {
+        $dir = new Connect();
+        $this->assertEquals($dir->getName(), 'dropbox_connect');
+    }
+
+    /**
+     * Tests that a file can be determined false
+     */
+    public function testRuleFail()
+    {
+        $val = new Validate();
+        
+        $val->rule('dropbox_connect', 'connection_field', array(
+            'dropbox_access_token' => 'fdsafdsa',
+            'dropbox_app_secret' => 'fdsafdsa'
+        ))->val(array(
+            'connection_field' => __FILE__
+        ));
+        $this->assertTrue($val->hasErrors());
+    }
+
+    /**
+     * Tests that a directory can be determined true
+     */
+    public function testRuleSuccess()
+    {
+        $val = new Validate();
+        $val->rule('dropbox_connect', 'connection_field', $this->getDropboxCreds())
+            ->val(array(
+            'connection_field' => 'Foo'
+        ));
+        $this->assertFALSE($val->hasErrors());
+    }
+    
+    /**
+     * The Dropbox API testing detalis
+     *
+     * @return array
+     */
+    protected function getDropboxCreds()
+    {
+        return include 'data/dropboxcreds.config.php';
+    }
+}
+
+$rule = new Connect;
+\JaegerApp\Validate::addrule($rule->getName(), array($rule, 'validate'), $rule->getErrorMessage());
